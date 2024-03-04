9d66bcdb7bf2d279a9789a2f486b3b09c4d86232
SatyamGuptaDev <devsatyamg@gmail.com>
2024-03-04 12:25:28 +0530

    Added routes and created all pages and add react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-black'>
-      <h1
-        className='text-4xl font-bold text-center text-blue-500'
-      >App</h1>
-      
+    <div
+      className="text-center bg-gray-500 min-h-screen flex flex-col font-bold "
+      style={{ fontFamily: "sans-serif" }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-out" element={<SignOut />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
