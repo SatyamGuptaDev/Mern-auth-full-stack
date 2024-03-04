8e4f722b32bcf7f1e9583f0fdd94d731f10eca4f
SatyamGuptaDev <devsatyamg@gmail.com>
2024-03-04 11:48:07 +0530

    Initialised react and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-black'>
+      <h1
+        className='text-4xl font-bold text-center text-blue-500'
+      >App</h1>
+      
+    </div>
+  )
+}
+
+export default App
