658d63178d1b7031f4d73c8d147a1c7d0219a0e6
SatyamGuptaDev <devsatyamg@gmail.com>
2024-04-13 22:59:03 +0530

    Update .gitignore file and connect to MongoDB in API index.js

--- api/index.js
+++ api/index.js
@@ -2,16 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Check if MONGO variable is properly loaded
 if (!process.env.MONGO) {
   console.error('MongoDB connection URI is not defined in the environment variables.');
-  process.exit(1); // Exit the process if MongoDB URI is not defined
+  process.exit(1); 
 }
 
-// Connect to MongoDB using the URI from environment variables
 mongoose.connect(process.env.MONGO).then(
   () => {
     console.log('Connected to MongoDB');
